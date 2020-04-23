2a75d7ed9a2fe7be55ff17f551426ee41fe9d636
Jeison Azevedo <j3ison@gmail.com>
2020-04-23 18:42:59 -0300

    fix typos

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { shade } from 'polished';
 
-import singUpBackground from '../../assets/sign-up-background.png';
+import signInBackground from '../../assets/sign-in-background.png';
 
 export const Container = styled.div`
   height: 100vh;
@@ -43,7 +43,7 @@ export const Content = styled.div`
   }
 
   > a {
-    color: #f4ede8;
+    color: #ff9000;
     display: block;
     margin-top: 24px;
     text-decoration: none;
@@ -56,13 +56,13 @@ export const Content = styled.div`
       margin-right: 16px;
     }
     &:hover {
-      color: ${shade(0.2, '#f4ede8')};
+      color: ${shade(0.2, '#ff9000')};
     }
   }
 `;
 
 export const Background = styled.div`
   flex: 1;
-  background: url(${singUpBackground}) no-repeat center;
+  background: url(${signInBackground}) no-repeat center;
   background-size: cover;
 `;
