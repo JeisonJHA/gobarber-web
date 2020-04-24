8339f7f8d811c373192a80586de2f3cc83aa528a
Jeison Azevedo <j3ison@gmail.com>
2020-04-23 23:51:21 -0300

    finish validations

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import * as Yup from 'yup';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
 
 import logo from '../../assets/logo.svg';
 
@@ -7,30 +10,50 @@ import Button from '../../components/Button';
 import Input from '../../components/Input';
 
 import { Container, Background, Content } from './styles';
+import getValidationErros from '../../utils/getValidationErros';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logo} alt="GoWeb" />
-      <form action="">
-        <h1>Faça seu login</h1>
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
-        <Button type="submit">Login</Button>
-        <a href="forgot">Esqueci a senha</a>
-      </form>
-      <a href="new">
-        <FiLogIn />
-        Criar conta
-      </a>
-    </Content>
-    <Background />
-  </Container>
-);
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail é obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Informe a senha'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+    } catch (err) {
+      const validations = getValidationErros(err);
+      formRef.current?.setErrors(validations);
+    }
+  }, []);
+  return (
+    <Container>
+      <Content>
+        <img src={logo} alt="GoWeb" />
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu login</h1>
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Login</Button>
+          <a href="forgot">Esqueci a senha</a>
+        </Form>
+        <a href="new">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
