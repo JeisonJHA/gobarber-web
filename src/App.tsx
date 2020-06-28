5483d46a700731f91e785648c1e355bc4e838d25
Jeison Azevedo <j3ison@gmail.com>
2020-06-28 02:43:06 -0300

    production env

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import 'dotenv/config';
+import dotenv from 'dotenv';
 import Routes from './routes';
 
 import GlobalStyles from './styles/global';
 import AppProvider from './hooks';
+
+dotenv.config({
+  path: process.env.NODE_ENV === 'development' ? '.env.dev' : '.env.production',
+});
 
 const App: React.FC = () => {
   return (
